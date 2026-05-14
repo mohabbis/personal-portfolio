e5ce90ac604f3bfb619532f58463b9c66b40bc4d
Muhammad Rafiq <101276427+mohabbis@users.noreply.github.com>
2026-05-14 10:54:44 -0500

    Add breathing room to page intros

--- components/sections/page-intro.tsx
+++ components/sections/page-intro.tsx
@@ -13,7 +13,7 @@ type PageIntroProps = {
 export function PageIntro({ eyebrow, title, description, actions }: PageIntroProps) {
   return (
     <section className="border-b border-border/70 bg-background">
-      <Container className="py-16 sm:py-20">
+      <Container className="py-20 sm:py-24 lg:py-28">
         <SectionHeading eyebrow={eyebrow} title={title} description={description} actions={actions} />
       </Container>
     </section>
