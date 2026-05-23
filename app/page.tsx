81dcf4eeaa4c7776dac89372baf3309ac4dda05a
Muhammad Rafiq <101276427+mohabbis@users.noreply.github.com>
2026-05-23 10:47:40 -0700

    Simplify homepage structure

--- app/page.tsx
+++ app/page.tsx
@@ -2,10 +2,8 @@ import type { Metadata } from "next";
 
 import { SiteFrame } from "@/components/layout/site-frame";
 import { HomeHero } from "@/components/sections/home-hero";
-import { CurrentSignalSection } from "@/components/sections/home/current-signal";
 import { HomeAboutSection } from "@/components/sections/home/home-about";
 import { HomeContactSection } from "@/components/sections/home/home-contact";
-import { HomeExperienceSection } from "@/components/sections/home/home-experience";
 import { HomeFeaturedWorkSection } from "@/components/sections/home/home-featured-work";
 import { siteConfig } from "@/data/site";
 
@@ -18,9 +16,7 @@ export default function HomePage() {
   return (
     <SiteFrame currentPath="/">
       <HomeHero />
-      <CurrentSignalSection />
       <HomeFeaturedWorkSection />
-      <HomeExperienceSection />
       <HomeAboutSection />
       <HomeContactSection />
     </SiteFrame>
